dd91a01fe4083fcc324092d47f7fda136ea7be9e
Trenton <markhamtrenton@gmail.com>
2021-07-18 20:16:01 -0400

    final update with everything wokring

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+mongoose.connect("mongodb+srv://trent:trenton64147@cluster0.awpyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
